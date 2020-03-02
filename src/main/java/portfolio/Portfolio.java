c71fe39d57eba59fc2738c9cf56cff1ce6290d8e
Maarten de Laat <maarten.delaat@students.fhnw.ch>
2020-03-02 15:04:24 +0100

    All Test implemented and run successfully

--- src/main/java/portfolio/Portfolio.java
+++ src/main/java/portfolio/Portfolio.java
@@ -1,5 +1,50 @@
 package portfolio;
 
-public class Portfolio {
+import portfolio.investments.Investment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Portfolio <T extends Investment> {
+    List<T> investments = new ArrayList<>();
+
+    public Portfolio() {
+
+    }
+
+    public boolean contains(T invest) {
+        return investments.contains(invest);
+    }
+
+    public void buy(T ubs) {
+        if (investments.contains(ubs)) {
+            for (T share : investments)
+                if (share.equals(ubs))
+                    share.setCount(share.getCount() + 1);
+        }
+        else
+            investments.add(ubs);
+    }
+
+    public void sell(String ubs, double amount) {
+        T  share = getShare(ubs);
+            if (share != null) {
+                share.setCount(share.getCount() - amount);
+                if (share.getCount() == 0) {
+                    share.setCount(0);
+                    investments.remove(share);
+                }
+            }
+    }
+
+    public T getShare(String ubs) {
+        for (T share : investments)
+            if (share.getTitel().equals(ubs))
+                return share;
+        return null;
+    }
+
+
+
     //TODO Complete this implementation and make it generic
 }
